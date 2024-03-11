ebb3996c5dd8b03bb856ddcfec3d576f0762dbd6
URLbug <gemslove2@gmail.com>
2024-03-11 22:15:39 +0200

    service file

---
+++ .top.menu.php
@@ -0,0 +1,11 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"УСЛУГИ", 
+		"/services/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ urlrewrite.php
@@ -0,0 +1,19 @@
+<?php
+$arUrlRewrite=array (
+  1 => 
+  array (
+    'CONDITION' => '#^/products/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/products/index.php',
+    'SORT' => 100,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/news/index.php',
+    'SORT' => 100,
+  ),
+);
