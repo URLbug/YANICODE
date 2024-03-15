4a77c1c0bb13980511293f36b2f438a58f6a2d7e
URLbug <gemslove2@gmail.com>
2024-03-15 18:28:49 +0200

    update all

---
+++ local/templates/yanicode/html/button_project.php
@@ -0,0 +1,5 @@
+<div class="button-wrapper  button-wrapper_center">
+    <div class="button button_gold button-open-calculate-project" data-popup="calculate-project-popup">
+        РАССЧИТАТЬ ПРОЕКТ
+    </div>
+</div>
--- services/index.php
+++ services/index.php
@@ -85,13 +85,15 @@ $APPLICATION->SetTitle("Услуги");
             )
         );
 
-        ?>
+        $APPLICATION->IncludeFile(
+            SITE_TEMPLATE_PATH . "/html/button_project.php",
+            [],
+            [
+                "MODE" => "html",
+            ]
+        );
 
-        <div class="button-wrapper  button-wrapper_center">
-            <div class="button button_gold button-open-calculate-project" data-popup="calculate-project-popup">
-                РАССЧИТАТЬ ПРОЕКТ
-            </div>
-        </div>
+        ?>
     </div>
 </main>
 
